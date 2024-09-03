f7924149fd5db65dc9db678b320451837e1fe1cc
Buuhuipofangle <buhuipofangle@qq.com>
2024-09-03 21:51:34 +0800

    done

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-var daily_str = `销售日报
-天气:—…小雨转多云☁…—
-日期:9/3
-店铺:石家庄2店怀旧杂货铺
-今日目标:4000
-明日目标:4000
-今日业绩：4000
-今日达标率：100.00%
-本周目标:32000
-本周累计完成：0
-本周达标率：15.85%
-本月目标:130000
-本月累计完成：0
-本月达标率：5.22%
-花茶：3421
-花茶月累计:0
-
-
-店铺日记:
-1. 今日目标未完成
-下午街上客流量少，都是零散进店，购买率低，晚上七点多街上客流慢慢变多一些，但进店率低，购买率低，导致业绩未完成。
-2.本月达标率5.22%，还差94.78%没达标，接下来每天要完成：4563.12的营业额才能达标。
-3.补货，整理排面，拍完排面，把零食柜子库存全部进行了收纳整理，玩具柜子没有整理，明天需要把后面零食柜子整理了，把缺的和少的货都拿了一些，整 
-理完库存，把灯罩门口窗户都清理了，把冰箱底部废水清理了，明天需要把方便面和饮料全都拿出来清理，网格货架需要好好擦一擦。`
 var _info = {}
 var _daily_arr = []
 var _tea = 3421
@@ -31,8 +7,8 @@ function excuse(sales,tea,daily){
     init(sales,tea,daily)
     handleDaily()
     handleDiary()
-    console.log(_daily_arr.join("\n"));
-    
+    return _daily_arr.join("\n");
+
 }
 function handleDaily(){
     set_value(_info.date,get_date())
@@ -62,7 +38,6 @@ function handleDaily(){
 }
 function handleDiary(){
     const diary_arr = _daily_arr[21].split("，")
-    
     
     diary_arr[0] = "2.本月达标率"+get_value(_info.monthly_ach);
     diary_arr[1] = "还差"+format(1 - parseFloat(get_value(_info.monthly_ach))/100)+"没达标"
@@ -119,6 +94,11 @@ function init(sales,tea,daily){
     _sales = sales
     _tea = tea
     _daily_arr = daily.split("\n");
+
+    if(_daily_arr.length != 24){
+        alert("格式过期 可能无法正确解析")
+    }
+
     _info = {
         date:2,
 
